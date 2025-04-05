c6aa4de489fa0772336b8d6d57f30b3b6bc47524
Robert Csokmai 912 <robert.csokmai@stud.ubbcluj.ro>
2025-04-05 13:30:44 +0300

    routes for landing page and register, made a request on beneficiar

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,26 @@ export default function App() {
         <Routes>
             <Route
                 path="/"
+                element={<div className="min-h-screen bg-gray-100 flex items-center justify-center">
+                    <h1 className="text-2xl font-bold">Bine ai venit!</h1>
+                </div>}
+            />
+            <Route
+                path="/register"
                 element={
                     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
                         <div className="bg-white p-6 rounded shadow-md w-96">
                             <h1 className="text-2xl font-bold mb-4">Înregistrare</h1>
                             <Register />
+                        </div>
+                    </div>
+                }
+            />
+            <Route
+                path="/login"
+                element={
+                    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+                        <div className="bg-white p-6 rounded shadow-md w-96">
                             <h1 className="text-2xl font-bold mb-4 mt-6">Login</h1>
                             <Login />
                         </div>
